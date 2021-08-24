32bbdae2efbcc4840056f494ed0999d6dd375531
Kai Koh <kaikoh95@gmail.com>
2021-08-24 15:19:17 +1200

    Develop: Updated Block methods and added Block struct.

--- src/blocks/Blocks.go
+++ src/blocks/Blocks.go
@@ -7,26 +7,37 @@ import (
 	"math/big"
 	"strconv"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
+type Block struct {
+	Number uint64
+	Difficulty uint64
+	Time uint64
+	Hash common.Hash
+	Hex string
+	LenTransactions int
+}
+
 func GetBlockHeader(client *ethclient.Client) *types.Header {
 	blockHeader, err := client.HeaderByNumber(context.Background(), nil)
     if err != nil {
-        log.Fatal(err)
+        log.Fatal("Unable to get block header ", err)
     }
 	fmt.Println("blockHeader number", blockHeader.Number.String()) // 5671744
 	return blockHeader
 }
 
-func FormatBlockDetails(block *types.Block) string {
+func (block Block) FormatBlockDetails() string {
 	format := "#############\n"
-	format += "block number " + strconv.FormatUint(block.Number().Uint64(), 10) + "\n"
-	format += "block number of transactions: " + strconv.Itoa(len(block.Transactions())) + "\n"
-	format += "block time " + strconv.FormatUint(block.Time(), 10) + "\n"
-	format += "block difficulty " + strconv.FormatUint(block.Difficulty().Uint64(), 10) + "\n"
-	format += "block hash hex " + block.Hash().Hex() + "\n"
+	format += "block number " + strconv.FormatUint(block.Number, 10) + "\n"
+	format += "block number of transactions: " + strconv.Itoa(block.LenTransactions) + "\n"
+	format += "block time " + strconv.FormatUint(block.Time, 10) + "\n"
+	format += "block difficulty " + strconv.FormatUint(block.Difficulty, 10) + "\n"
+	format += "block hex " + block.Hex + "\n"
+	format += "block hash " + block.Hash.String() + "\n"
 	format += "#############\n"
 	return format
 }
@@ -34,15 +45,15 @@ func FormatBlockDetails(block *types.Block) string {
 func GetFullBlock(client *ethclient.Client, blockNumber *big.Int) *types.Block {
 	block, err := client.BlockByNumber(context.Background(), blockNumber)
     if err != nil {
-        log.Fatal(err)
+        log.Fatal("Unable to get block from block number ", err)
     }
 	return block
 }
 
-func GetBlockTransactionCount(client *ethclient.Client, block *types.Block) uint {
-	count, err := client.TransactionCount(context.Background(), block.Hash())
+func GetBlockTransactionCount(client *ethclient.Client, hash common.Hash) uint {
+	count, err := client.TransactionCount(context.Background(), hash)
     if err != nil {
-        log.Fatal(err)
+        log.Fatal("Unable to get transaction count of block ", err)
     }
 	return count
 }
