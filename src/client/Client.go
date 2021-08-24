3922961f3e48bd7cb17b0369d15b6a9d4173b510
Kai Koh <kaikoh95@gmail.com>
2021-08-24 15:18:36 +1200

    Develop: Added initclient with default testnet.

--- src/client/Client.go
+++ src/client/Client.go
@@ -6,10 +6,14 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func InitClient(network string) *ethclient.Client {
+func Init(network string) *ethclient.Client {
     client, err := ethclient.Dial(network)
     if err != nil {
         log.Fatal(err)
     }
     return client
+}
+
+func InitWithDefaultNetwork() *ethclient.Client {
+    return Init("https://rinkeby.infura.io/v3/7073cc887d0449feaf3017cc7bc6090e")
 }
