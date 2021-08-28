46dd93bd8b173b7a0f63f11249e9d61abdb5aec1
Kai Koh <kaikoh95@gmail.com>
2021-08-28 15:35:10 +1200

    Refactored code.

--- src/main.go
+++ src/main.go
@@ -1,30 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"math/big"
 
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
-	Account "github.com/kaikoh95/web3go/src/account"
-	Client "github.com/kaikoh95/web3go/src/client"
-	HdWallet "github.com/kaikoh95/web3go/src/hdwallet"
+	"github.com/kaikoh95/web3go/src/Account"
+	"github.com/kaikoh95/web3go/src/client"
+	"github.com/kaikoh95/web3go/src/hdwallet"
 
-	// Helpers "github.com/kaikoh95/web3go/src/helpers"
+	"github.com/kaikoh95/web3go/src/helpers"
 
-	// Blocks "github.com/kaikoh95/web3go/src/blocks"
-	Transactions "github.com/kaikoh95/web3go/src/transactions"
-	// Keystores "github.com/kaikoh95/web3go/src/keystores"
-	// Wallet "github.com/kaikoh95/web3go/src/wallet"
+	"github.com/kaikoh95/web3go/src/transactions"
 )
 
 func main() {
 	// get eth-ropsten from faucet
 	// https://faucet.dimensions.network/
 
-	client := Client.InitWithDefaultNetwork()
+	client := client.InitWithDefaultNetwork()
 	fmt.Println("we have a connection", client)
 
-	chainID := Transactions.GetClientChainID(client)
+	chainID := transactions.GetClientChainID(client)
 	fmt.Println("chain ID", chainID)
+	
+	// networkUrl := "https://ropsten.etherscan.io"
+	networkUrl := "https://rinkeby.etherscan.io"
+	fmt.Println("network url", networkUrl)
+
+	////////////// start test code //////////////
+
 
 	////////////// end test code //////////////
 
@@ -49,49 +57,49 @@ func main() {
 	// fmt.Println("pending balance", pendingBalance)
 
 	///// Wallet setup
-	// privateKey := Wallet.GeneratePrivateKey()
+	// privateKey := wallet.GeneratePrivateKey()
     // fmt.Println("privateKey ", privateKey)
 	
-	// privateKeyBytes := Wallet.GetPrivateKeyBytes(privateKey)
+	// privateKeyBytes := wallet.GetPrivateKeyBytes(privateKey)
     // fmt.Println("privateKeyBytes ", privateKeyBytes)
 	
-	// privateKeyHex := Wallet.GetPrivateKeyHex(privateKey)
+	// privateKeyHex := wallet.GetPrivateKeyHex(privateKey)
     // fmt.Println("privateKeyHex ", privateKeyHex)
 	
-	// publicKey := Wallet.GetPublicKey(privateKey)
+	// publicKey := wallet.GetPublicKey(privateKey)
     // fmt.Println("publicKey ", publicKey)
 	
-	// publicKeyECDSA := Wallet.GetPublicKeyECDSA(publicKey)
+	// publicKeyECDSA := wallet.GetPublicKeyECDSA(publicKey)
 	// fmt.Println("publicKeyECDSA ", publicKeyECDSA)
 
-	// // publicKeyHex := Wallet.GetPublicKeyHex(publicKeyECDSA)
+	// // publicKeyHex := wallet.GetPublicKeyHex(publicKeyECDSA)
 	// // fmt.Println("publicKeyHex ", publicKeyHex)
 	
-	// publicAddress := Wallet.GetPublicAddress(publicKeyECDSA)
+	// publicAddress := wallet.GetPublicAddress(publicKeyECDSA)
 	// fmt.Println("publicAddress ", publicAddress)
 
 	// ///// Keystores setup
 	// var account accounts.Account
-	// ks := Keystores.InitKeyStore("./wallets")
+	// ks := keystores.InitKeyStore("./wallets")
 	// password := "password"
 	
 	///// Create new account 
-	// account = Keystores.CreateNewAccount(ks, password)
+	// account = keystores.CreateNewAccount(ks, password)
 	
 	// ///// Import account
 	// file := "./wallets/UTC--2021-08-21T23-38-28.676160000Z--896562a998b4b819f23c05dc78c39e6f43f70b3d"
-	// account =  Keystores.ImportAccountFromKeyStore(ks, file, password)
+	// account =  keystores.ImportAccountFromKeyStore(ks, file, password)
 	
 	// accountAddressFromKeystores := Account.GetAccountAddress(account)
 	// fmt.Println("address hex ", Account.GetAccountAddressHex(accountAddressFromKeystores))
 
 	///// Blocks
-	// blockHeader := Blocks.GetBlockHeader(client)
+	// blockHeader := blocks.GetBlockHeader(client)
 	// fmt.Println("blockHeader number", blockHeader.Number.String()) // 5671744
 	// var blockNumber *big.Int
     // blockNumber = big.NewInt(5671744)
-    // block := Blocks.GetFullBlock(client, blockNumber)
-	// blockStruct := Blocks.Block{
+    // block := blocks.GetFullBlock(client, blockNumber)
+	// blockStruct := blocks.Block{
 	// 	Number: block.Number().Uint64(),
 	// 	Difficulty: block.Difficulty().Uint64(),
 	// 	Time: block.Time(),
@@ -105,38 +113,39 @@ func main() {
 	///// Transactions
 	// withReceipt := true
     // blockNumber = big.NewInt(5671744)
-    // block := Blocks.GetFullBlock(client, blockNumber)
-	// transactions := Transactions.GetTransactionDetails(client, block, withReceipt)
+    // block := blocks.GetFullBlock(client, blockNumber)
+	// transactions := transactions.GetTransactionDetails(client, block, withReceipt)
 	// fmt.Println(transactions)
 	
-	// blockHash := Transactions.GetHashFromHex(block.Hash().Hex())
+	// blockHash := transactions.GetHashFromHex(block.Hash().Hex())
 	// fmt.Println("block hash ", blockHash)
-	// transactionsFromBlockHash := Transactions.GetTransactionsFromBlockHash(blockHash, client, withReceipt)
+	// transactionsFromBlockHash := transactions.GetTransactionsFromBlockHash(blockHash, client, withReceipt)
 	// fmt.Println(transactionsFromBlockHash)
 	
 	// transactionHex := "0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2"
 	// fmt.Println("transactionHex ", transactionHex)
-    // transactionHash := Transactions.GetHashFromHex(transactionHex)
-	// singleTransactionFromTransactionHash := Transactions.GetSingleTransactionFromTransactionHash(transactionHash, client, withReceipt)
+    // transactionHash := transactions.GetHashFromHex(transactionHex)
+	// singleTransactionFromTransactionHash := transactions.GetSingleTransactionFromTransactionHash(transactionHash, client, withReceipt)
 	// fmt.Println("singleTransactionFromTransactionHash ", singleTransactionFromTransactionHash)
 
 	///// HDWallets
-	// ryanAddr := Account.GetAddressFromHex("0x3AbbBD290D35ea69E62eE1b5D45da6934490DC34")
+	ryanAddr := Account.GetAddressFromHex("0x3AbbBD290D35ea69E62eE1b5D45da6934490DC34")
+	fmt.Println("Ryan address ", ryanAddr)
 
 	// wallet to act as receiver
 	recMnemonic := "cruel twelve firm dignity huge such boost vault meadow monkey grace outer element cruise danger live benefit morning draft fury someone fall rich pride"
-	recWallet := HdWallet.GenerateHdWalletFromMnemonic(recMnemonic)
-	recPath := HdWallet.ParsedPathFromDefaultDerivationPath()
-	recAccount := HdWallet.DeriveAccountFromParsedPath(recWallet, recPath)
+	recWallet := hdwallet.GenerateHdWalletFromMnemonic(recMnemonic)
+	recPath := hdwallet.ParsedPathFromDefaultDerivationPath()
+	recAccount := hdwallet.DeriveAccountFromParsedPath(recWallet, recPath)
 	recPub, _ := recWallet.PublicKey(recAccount)
 	recPubAddr := crypto.PubkeyToAddress(*recPub)
 	fmt.Println("Receiver address ", recPubAddr)
 
 	// wallet to act as sender
 	mnemonic := "man drastic shed trip rug extra bar trophy sign floor vibrant step square hour clap file brown black cable seminar squirrel holiday negative brain"
-	wallet1 := HdWallet.GenerateHdWalletFromMnemonic(mnemonic)
-	path := HdWallet.ParsedPathFromDefaultDerivationPath()
-	account1 := HdWallet.DeriveAccountFromParsedPath(wallet1, path)
+	wallet1 := hdwallet.GenerateHdWalletFromMnemonic(mnemonic)
+	path := hdwallet.ParsedPathFromDefaultDerivationPath()
+	account1 := hdwallet.DeriveAccountFromParsedPath(wallet1, path)
 	
 	privateKey, _ := wallet1.PrivateKey(account1)
 	fmt.Println("pk ", privateKey)
@@ -153,66 +162,104 @@ func main() {
 	// fmt.Println("balance in wei", weiBal)
 	
 	// ///// Send ETH
-	// amountToSend := 0.005
-	// weiAmountToSend := Helpers.ToWei(amountToSend, 18)
-	// fmt.Println("amount to send in wei", weiAmountToSend)
-	// gasLimit := Helpers.EstimateGasLimit(client, &recPubAddr, nil)
-	// gasPrice := Helpers.SuggestGasPrice(client)
-	// gasCost := Helpers.CalcGasCost(gasLimit, gasPrice)
+	amountToSend := 0.0005
+	weiAmountToSend := helpers.ToWei(amountToSend, 18)
+	fmt.Println("amount to send in wei", weiAmountToSend)
+	// gasLimit := helpers.EstimateGasLimit(client, &recPubAddr, nil)
+	gasPrice := helpers.SuggestGasPrice(client)
+	// gasCost := helpers.CalcGasCost(gasLimit, gasPrice)
 	// fmt.Println("gas cost in wei ", gasCost)
 
-	// /// nil for just sending ETH 
+	//// nil for just sending ETH 
+	// Legacy Tx
 	// tx := types.NewTransaction(nonce, recPubAddr, weiAmountToSend, gasLimit, gasPrice, nil)
-
-	// signedTx, err := types.SignTx(tx, types.LatestSignerForChainID(chainID), privateKey)
-	// if err != nil {
-	// 	log.Fatal("Failed to sign transaction ", err)
-	// }
-
-	// err = client.SendTransaction(context.Background(), signedTx)
-	// if err != nil {
-	// 	log.Fatal("Unable to send transaction ", err)
-	// }
-
-	// fmt.Printf("tx sent: %s \n", signedTx.Hash().Hex())
-	// ///// fmt.Println(spew.Dump(signedTx))
-
-	// ///// send ERC-20 Token
-	// SatorV2 := "0xaced0798cbA611f1613BaD8E6dC0Ac30C2C4Bb66"
-	// tokenAddress := Account.GetAddressFromHex(SatorV2)
-	// fmt.Println(tokenAddress)
-
-	// methodName := "transfer(address,uint256)"
-	// tokenAmountToSend := 1000
-	// data := Transactions.PrepareTokenTransactionData(methodName, &recPubAddr, tokenAmountToSend)
-	// gasPrice := Helpers.SuggestGasPrice(client)
-	// gasLimit := Helpers.EstimateGasLimit(client, &recPubAddr, data)
-	// fmt.Println("gas ", gasLimit) // 23256
-	// fmt.Println("gas price", gasPrice) // 23256
-	// fmt.Println("nonce ", nonce)
 	
-	// dynamicTx := types.DynamicFeeTx{
+	//// EIP-1559 Tx
+	// ethPrepTx := types.DynamicFeeTx{
 	// 	ChainID: chainID,
 	// 	Nonce: nonce,
 	// 	GasFeeCap: gasPrice,
 	// 	GasTipCap: gasPrice,
 	// 	Gas: gasLimit,
-	// 	To: &tokenAddress,
-	// 	Value: big.NewInt(0),
-	// 	Data: data,
+	// 	To: &recPubAddr,
+	// 	Value: weiAmountToSend,
 	// }
-	// tx := types.NewTx(&dynamicTx)
 
-	// signedTx, err := types.SignTx(tx, types.LatestSignerForChainID(chainID), privateKey)
-    // if err != nil {
-    //     log.Fatal(err)
-    // }
-	// err = client.SendTransaction(context.Background(), signedTx)
+	//// EIP-2930 Tx
+	// var storageKeys []common.Hash
+	
+	// storageKeys = append(storageKeys, accountAddress1.Hash())
+	// accessTuple := types.AccessTuple{
+	// 	Address: accountAddress1,
+	// 	StorageKeys: storageKeys,
+	// }
+	// var accessList []types.AccessTuple
+	// accessList = append(accessList, accessTuple)
+	// ethPrepTx := types.AccessListTx{
+	// 	ChainID: chainID,
+	// 	Nonce: nonce,
+	// 	GasPrice: gasPrice,
+	// 	Gas: uint64(800000),
+	// 	To: &recPubAddr,
+	// 	Value: weiAmountToSend,
+	// 	AccessList: accessList,
+	// }
+	// ethTx := types.NewTx(&ethPrepTx)
+
+	// signedEthTx, err := types.SignTx(ethTx, types.NewEIP2930Signer(chainID), privateKey)
+	// if err != nil {
+	// 	log.Fatal("Failed to sign transaction ", err)
+	// }
+
+	// err = client.SendTransaction(context.Background(), signedEthTx)
 	// if err != nil {
 	// 	log.Fatal("Unable to send transaction ", err)
 	// }
-	// fmt.Printf("tx hex %s \n", signedTx.Hash().Hex())
-	// networkUrl := "https://ropsten.etherscan.io"
-	// fmt.Printf("check tx here %s/tx/%s \n", networkUrl, signedTx.Hash().Hex())
+
+	// fmt.Printf("check tx here %s/tx/%s \n", networkUrl, signedEthTx.Hash().Hex())
+	// ///// fmt.Println(spew.Dump(signedEthTx))
+
+	// ///// send ERC-20 Token
+
+	//// Ropsten
+	//// SatorV2 := "0xaced0798cbA611f1613BaD8E6dC0Ac30C2C4Bb66"
+	
+	//// Rinkeby
+	// SatorV2 := "0xb082ee966A31089b08bcE68Ec43C0aB580be2A8a"
+	Sator777 := "0x5794E45E4E06C099C0873c633C41943610DBfBe2"
+	tokenAddress := Account.GetAddressFromHex(Sator777)
+	fmt.Println(tokenAddress)
+
+	methodName := "transfer(address,uint256)"
+	tokenAmountToSend := 1000
+	data := transactions.PrepareTokenTransactionData(
+		methodName, &recPubAddr, tokenAmountToSend)
+	// gasPrice := helpers.SuggestGasPrice(client)
+	gasLimit := helpers.EstimateGasLimit(client, &recPubAddr, data)
+	fmt.Println("gas ", gasLimit) // 23256
+	fmt.Println("gas price", gasPrice) // 23256
+	fmt.Println("nonce ", nonce)
+	dynamicTx := types.DynamicFeeTx{
+		ChainID: chainID,
+		Nonce: nonce,
+		GasFeeCap: gasPrice.Mul(gasPrice, big.NewInt(50)),
+		GasTipCap: gasPrice.Mul(gasPrice, big.NewInt(50)),
+		Gas: gasLimit * 20,
+		To: &tokenAddress,
+		Value: big.NewInt(0),
+		Data: data,
+	}
+	tx := types.NewTx(&dynamicTx)
+
+	signedTx, err := types.SignTx(tx, types.LatestSignerForChainID(chainID), privateKey)
+    if err != nil {
+        log.Fatal(err)
+    }
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		log.Fatal("Unable to send transaction ", err)
+	}
+	fmt.Printf("tx hex %s \n", signedTx.Hash().Hex())
+	fmt.Printf("check tx here %s/tx/%s \n", networkUrl, signedTx.Hash().Hex())
 	
 }
